824a467dfda871d2bf89300831fdee79eb9cf4dd
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-08-24 21:21:20 -0600

    feat: Se cargo la interfaz de la entidad Usuario con sus atributos

---
+++ EcoMuniGT/Backend/src/models/usuario.ts
@@ -0,0 +1,11 @@
+export interface Usuario{
+    idUsuario: number
+    idRol:number
+    nombres: String,
+    apellidos: String,
+    correo: String,
+    contrasena: String, 
+    telefono: String,
+    estado: String, 
+    fechaRegistro: Date
+}
