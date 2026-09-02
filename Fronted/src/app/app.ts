221b30fb5029f4a81dad72e672842bca94d3bb6d
dromero-2025157 <dromero-2025157@kinal.edu.gt>
2026-09-02 09:49:49 -0600

    feat:Se ha agregado el componentes del reguister

--- Fronted/src/app/app.ts
+++ Fronted/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ReguisterComponent } from './Components/Reguister';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.css',
+  standalone: true,
+  imports: [ReguisterComponent], // <-- Agrégalo aquí
   templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Fronted');
