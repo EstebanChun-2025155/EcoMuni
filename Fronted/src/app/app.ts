95ba62dc0ab4a42521a0e70e7d0296ce0fece684
domingoromero-2025157 <dromero-2025157@kinal.edu.gt>
2026-09-09 08:11:05 -0600

    Feat:se a modificado app.ts

--- Fronted/src/app/app.ts
+++ Fronted/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { ReguisterComponent } from './Components/Reguister';
+import { ReguisterComponent } from './Components/register/Reguister';
+import { HomeComponent } from './Components/home/Home';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ReguisterComponent], // <-- Agrégalo aquí
+  imports: [HomeComponent, ReguisterComponent], // <-- Agrégalo aquí
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
