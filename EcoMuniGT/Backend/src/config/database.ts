811b9403b9fcc2f98d4f00afb28877cd864b7843
luischac-2025089 <lchac-2025089@kinal.edu.gt>
2026-09-14 11:51:53 -0600

    fix: se corrigieron inconsistencias en database.ts

--- EcoMuniGT/Backend/src/config/database.ts
+++ EcoMuniGT/Backend/src/config/database.ts
@@ -3,9 +3,9 @@ import { Pool } from "pg";
 export const pool = new Pool({
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT || 5432),
-    database: process.env.DB_NAME || "db_ecomuni",
+    database: process.env.DB_NAME || "db-EcoMuni",
     user: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD 
+    password: process.env.DB_PASSWORD || "admin"
 });
 
 export async function probarConexion(): Promise<boolean> {
