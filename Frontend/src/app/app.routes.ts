440a8dc4a745afa4e026fd5185b1d252aa31d2b8
echun-2025155 <echun-2025155@kinal.edu.gt>
2026-09-16 11:52:58 -0600

    feat: se registraron rutas para vistas de departamentos

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,12 +2,35 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { HomeComponent } from './components/home/home';
 import { RegisterComponent } from './components/register/register';
+import { DepartamentoComponent } from './components/departamento/departamento';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [authGuard]},
-  { path: 'register', component: RegisterComponent },
-  { path: '**', redirectTo: 'login' }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'departamentos/:slug',
+    component: DepartamentoComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
 ];
