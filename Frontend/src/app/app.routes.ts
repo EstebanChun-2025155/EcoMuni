aeb3d803ea3fa33325c8aab05ed1cdbd9b6f67bc
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-09-15 18:34:46 -0600

    feat: se configuraron rutas de login, registro y home protegida

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login';
+import { HomeComponent } from './components/home/home';
+import { RegisterComponent } from './components/register/register';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard]},
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: 'login' }
+];
