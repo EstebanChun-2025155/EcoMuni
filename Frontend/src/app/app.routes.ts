016717753ebbfaf1df86caaf5024c8b8de45ebcd
Estebanchun-2025155 <echun-2025155@kinal.edu.gt>
2026-09-18 22:31:50 -0600

    feat: se configuraron la rutas independientes para los cinco departamentos

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { HomeComponent } from './components/home/home';
 import { RegisterComponent } from './components/register/register';
-import { DepartamentoComponent } from './components/departamento/departamento';
+
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -21,10 +21,31 @@ export const routes: Routes = [
     canActivate: [authGuard]
   },
   {
-    path: 'departamentos/:slug',
-    component: DepartamentoComponent,
+    path: 'departamentos/guatemala',
+    loadComponent: () => import('./components/cdguatemala/cdguatemala').then(m => m.CDGuatemala),
     canActivate: [authGuard]
   },
+  {
+    path: 'departamentos/huehuetenango',
+    loadComponent: () => import('./components/huehuetenango/huehuetenango').then(m => m.Huehuetenango),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'departamentos/izabal',
+    loadComponent: () => import('./components/izabal/izabal').then(m => m.Izabal),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'departamentos/jalapa',
+    loadComponent: () => import('./components/jalapa/jalapa').then(m => m.Jalapa),
+    canActivate: [authGuard]
+  },
+  {
+    path: 'departamentos/jutiapa',
+    loadComponent: () => import('./components/jutiapa/jutiapa').then(m => m.Jutiapa),
+    canActivate: [authGuard]
+  },
+  { path: 'departamentos/:slug', redirectTo: 'home' },
   {
     path: 'register',
     component: RegisterComponent
