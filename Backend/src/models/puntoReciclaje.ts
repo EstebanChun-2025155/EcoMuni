bbafe9d3449ccd824ee7f16e787c6e97e0dea4ba
domingoromero-2025157 <dromero-2025157@kinal.edu.gt>
2026-09-20 08:49:06 -0600

    feat: agregar modelo para puntos de reciclaje

---
+++ Backend/src/models/puntoReciclaje.ts
@@ -0,0 +1,9 @@
+export interface PuntoReciclaje {
+    idPunto: number;
+    nombre: string;
+    ubicacion: string;
+    materiales: string;
+    descripcion: string | null;
+    horario: string | null;
+    telefono: string | null;
+}
