c27f6bbec422b9486f1d7372c9f5e0154e287322
domingoromero-2025157 <dromero-2025157@kinal.edu.gt>
2026-09-19 23:05:30 -0600

    feat: implementar modulo y vistas especificas para Quiche

---
+++ Fronted/src/app/Components/quiche/quiche.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DepartamentoPagina } from '../departamentos-shared/departamento-pagina';
+
+@Component({
+  selector: 'app-quiche',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './quiche.html',
+  styleUrl: './quiche.css'
+})
+export class Quiche extends DepartamentoPagina {
+  constructor() {
+    super({
+      "slug": "quiche",
+      "nombre": "Quiché",
+      "cabecera": "Santa Cruz del Quiché",
+      "municipios": 21,
+      "imagen": "/Quiche.png",
+      "frase": "Tierra de historia, tradiciones sagradas y comunidades unidas por la protección de sus bosques y valles."
+    });
+  }
+}
