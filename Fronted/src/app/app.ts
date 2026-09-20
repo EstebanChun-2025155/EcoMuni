15f6993e787b8d4b3ae957eb15b664a7cd4b283b
domingoromero-2025157 <dromero-2025157@kinal.edu.gt>
2026-09-20 10:36:45 -0600

    refactor: actualizar componente principal de la aplicación

--- Fronted/src/app/app.ts
+++ Fronted/src/app/app.ts
@@ -2,12 +2,25 @@ import { Component, signal } from '@angular/core';
 import { ReguisterComponent } from './Components/register/Reguister';
 import { HomeComponent } from './Components/home/Home';
 import { LocationsComponent } from './Components/Locations/Locations';
-
+import { Quiche } from './Components/quiche/quiche';
+import { Quetzaltenango } from './Components/Quetzaltenango/quetzaltenango';
+import { Peten } from './Components/Peten/Peten'; // Fixed typo: Peten (was Petent)
+import { Retalhuleu } from './Components/Retalhuleu/retalhuleu';
+import { Sacatepequez } from './Components/sacatepequez/sacatepequez';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LocationsComponent,HomeComponent, ReguisterComponent], // <-- Agrégalo aquí
+  imports: [
+    LocationsComponent,
+    HomeComponent,
+    ReguisterComponent,
+    Quiche,
+    Quetzaltenango,
+    Peten,
+    Retalhuleu,
+    Sacatepequez
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
