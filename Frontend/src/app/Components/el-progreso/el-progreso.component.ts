9ab491b6902789b88c840358699606b082436a36
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-09-21 11:54:16 -0600

    feat:Se ha agregado el departamento de El Progreso con su diseño actualizado

---
+++ Frontend/src/app/Components/el-progreso/el-progreso.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DepartamentoPagina } from '../departamentos-shared/departamento-pagina';
+
+@Component({
+  selector: 'app-el-progreso',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './el-progreso.component.html',
+  styleUrl: './el-progreso.component.css'
+})
+export class ElProgresoComponent extends DepartamentoPagina {
+  constructor() {
+    super({
+      "slug": "el-progreso",
+      "nombre": "El Progreso",
+      "cabecera": "El Progreso",
+      "municipios": 7,
+      "imagen": "/El-Progreso.png",
+      "frase": "Tradición, naturaleza y comunidades comprometidas con un futuro más verde."
+    });
+  }
+}
