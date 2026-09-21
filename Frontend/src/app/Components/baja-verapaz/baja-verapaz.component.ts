bf4e8c11314ce61ac2a6990eb3129f410dcca90e
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-09-21 11:50:42 -0600

    feat:Se ha agregado el departamento de Baja Verapaz con su diseño actualizado

---
+++ Frontend/src/app/Components/baja-verapaz/baja-verapaz.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DepartamentoPagina } from '../departamentos-shared/departamento-pagina';
+
+@Component({
+  selector: 'app-baja-verapaz',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './baja-verapaz.component.html',
+  styleUrl: './baja-verapaz.component.css'
+})
+export class BajaVerapazComponent extends DepartamentoPagina {
+  constructor() {
+    super({
+      "slug": "baja-verapaz",
+      "nombre": "Baja Verapaz",
+      "cabecera": "Baja Verapaz",
+      "municipios": 7,
+      "imagen": "/Baja-Verapaz.png",
+      "frase": "Tradición, naturaleza y comunidades comprometidas con un futuro más verde."
+    });
+  }
+}
