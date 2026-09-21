52612a6649b63a477ec8966b51c1a188463c681a
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-09-21 11:54:54 -0600

    feat:Se ha agregado el departamento de Escuintla con su diseño actualizado

---
+++ Frontend/src/app/Components/escuintla/escuintla.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DepartamentoPagina } from '../departamentos-shared/departamento-pagina';
+
+@Component({
+  selector: 'app-escuintla',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './escuintla.component.html',
+  styleUrl: './escuintla.component.css'
+})
+export class EscuintlaComponent extends DepartamentoPagina {
+  constructor() {
+    super({
+      "slug": "escuintla",
+      "nombre": "Escuintla",
+      "cabecera": "Escuintla",
+      "municipios": 7,
+      "imagen": "/Escuintla.png",
+      "frase": "Tradición, naturaleza y comunidades comprometidas con un futuro más verde."
+    });
+  }
+}
