5ad25dcf0bdc45842d00be1b7764523acb1ca32a
domingoromero-2025157 <dromero-2025157@kinal.edu.gt>
2026-09-21 11:45:12 -0600

    refactor: actualizar configuración de la conexión a la base de datos

--- Backend/src/config/database.ts
+++ Backend/src/config/database.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 export const pool = new Pool({
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT || 5432),
-    database: process.env.DB_NAME || "db_ecomuni",
+    database: process.env.DB_NAME || "postgres",
     user: process.env.DB_USER || "postgres",
     connectionTimeoutMillis: 5000,
     password: process.env.DB_PASSWORD 
