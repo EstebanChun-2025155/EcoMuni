aa3d741f05edfd2e671f16617e74b8751fafe0be
domingoromero-2025157 <dromero-2025157@kinal.edu.gt>
2026-09-20 22:49:32 -0600

    refactor: actualizar componente raiz de la aplicacion

--- Fronted/src/app/app.ts
+++ Fronted/src/app/app.ts
@@ -1,29 +1,4 @@
-import { Component, signal } from '@angular/core';
-import { ReguisterComponent } from './Components/register/Reguister';
-import { HomeComponent } from './Components/home/Home';
-import { LocationsComponent } from './Components/Locations/Locations';
-import { Quiche } from './Components/quiche/quiche';
-import { Quetzaltenango } from './Components/Quetzaltenango/quetzaltenango';
-import { Peten } from './Components/Peten/Peten'; // Fixed typo: Peten (was Petent)
-import { Retalhuleu } from './Components/Retalhuleu/retalhuleu';
-import { Sacatepequez } from './Components/sacatepequez/sacatepequez';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    LocationsComponent,
-    HomeComponent,
-    ReguisterComponent,
-    Quiche,
-    Quetzaltenango,
-    Peten,
-    Retalhuleu,
-    Sacatepequez
-  ],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected readonly title = signal('Fronted');
-}
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+@Component({selector:'app-root', standalone:true, imports:[RouterOutlet], templateUrl:'./app.html', styleUrl:'./app.css'})
+export class App {}
