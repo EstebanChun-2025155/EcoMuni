4ae5dd17eccd195623d558d937d7ef3490888afd
domingoromero-2025157 <dromero-2025157@kinal.edu.gt>
2026-09-20 22:36:38 -0600

    refactor: actualizar modelo de categoria

--- Backend/src/models/categoria.ts
+++ Backend/src/models/categoria.ts
@@ -1,6 +1,6 @@
 export interface Categoria {
-    idCategoria: number;
-    categoria: string;
-    descripcion: string;
-    estado: string;
+ idCategoria?: number;
+ categoria: string;
+ descripcion: string | null;
+ estado: "activa" | "inactiva";
 }
