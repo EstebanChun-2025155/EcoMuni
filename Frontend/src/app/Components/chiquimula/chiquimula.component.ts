a158ef6245bd3cce033f677298d60d5d09cd7dab
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-09-21 11:51:56 -0600

    feat:Se ha agregado el departamento de Chiquimula con su diseño actualizado

---
+++ Frontend/src/app/Components/chiquimula/chiquimula.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DepartamentoPagina } from '../departamentos-shared/departamento-pagina';
+
+@Component({
+  selector: 'app-chiquimula',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './chiquimula.component.html',
+  styleUrl: './chiquimula.component.css'
+})
+export class ChiquimulaComponent extends DepartamentoPagina {
+  constructor() {
+    super({
+      "slug": "chiquimula",
+      "nombre": "Chiquimula",
+      "cabecera": "Chiquimula",
+      "municipios": 7,
+      "imagen": "/Chiquimula.png",
+      "frase": "Tradición, naturaleza y comunidades comprometidas con un futuro más verde."
+    });
+  }
+}
