ed2b8f3bfece74ca7f43279847e01d00049d8584
luischac-2025089 <lchac-2025089@kinal.edu.gt>
2026-09-20 22:14:15 -0600

    refactor: se modificaron rutas

--- EcoMuniGT/Frontend/src/app/app.routes.ts
+++ EcoMuniGT/Frontend/src/app/app.routes.ts
@@ -1,3 +1,54 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'departamentos/san-marcos',
+    loadComponent: () =>
+      import('./Components/san-marcos/san-marcos')
+        .then(m => m.SanMarcos)
+  },
+  {
+    path: 'departamentos/santa-rosa',
+    loadComponent: () =>
+      import('./Components/santa-rosa/santa-rosa')
+        .then(m => m.SantaRosa)
+  },
+  {
+    path: 'departamentos/solola',
+    loadComponent: () =>
+      import('./Components/solola/solola')
+        .then(m => m.Solola)
+  },
+  {
+    path: 'departamentos/suchitepequez',
+    loadComponent: () =>
+      import('./Components/suchitepequez/suchitepequez')
+        .then(m => m.Suchitepequez)
+  },
+  {
+    path: 'departamentos/totonicapan',
+    loadComponent: () =>
+      import('./Components/totonicapan/totonicapan')
+        .then(m => m.Totonicapan)
+  },
+  {
+    path: 'departamentos/zacapa',
+    loadComponent: () =>
+      import('./Components/zacapa/zacapa')
+        .then(m => m.Zacapa)
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
