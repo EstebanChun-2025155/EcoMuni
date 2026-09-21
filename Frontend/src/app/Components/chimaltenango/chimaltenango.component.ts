63e5b183c88f096d387c0fe22c9f72db5b0c30be
aportillo-2022361 <aportillo-2022361@kinal.edu.gt>
2026-09-21 11:51:35 -0600

    feat:Se ha agregado el departamento de Chimaltenango con su diseño actualizado

---
+++ Frontend/src/app/Components/chimaltenango/chimaltenango.component.ts
@@ -0,0 +1,23 @@
+﻿import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DepartamentoPagina } from '../departamentos-shared/departamento-pagina';
+
+@Component({
+  selector: 'app-chimaltenango',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './chimaltenango.component.html',
+  styleUrl: './chimaltenango.component.css'
+})
+export class ChimaltenangoComponent extends DepartamentoPagina {
+  constructor() {
+    super({
+      "slug": "chimaltenango",
+      "nombre": "Chimaltenango",
+      "cabecera": "Chimaltenango",
+      "municipios": 7,
+      "imagen": "/Chimaltenango.png",
+      "frase": "Tradición, naturaleza y comunidades comprometidas con un futuro más verde."
+    });
+  }
+}
